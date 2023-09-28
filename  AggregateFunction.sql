829e8976f3f3ec7782c3df61307a30a3dbab4c2e
Nam Nguyen <nguyentnam.061297@gmail.com>
2023-09-28 19:14:12 +0700

    123

---  AggregateFunction.sql
+++  AggregateFunction.sql
@@ -35,4 +35,46 @@ select film.title, category.name from film join film_category on film.film_id = 
 join category on film_category.category_id = category.category_id
 where category.name like 'Games';
 
+-- Bài tập về nhà
+-- 1. Lấy danh sách tên các diễn viên (actors) có họ bắt đầu bằng chữ “S”.
+select first_name, last_name from actor where first_name like 'S%';
 
+-- 2. Lấy danh sách các bộ phim (films) được phát hành trong khoảng từ năm 2000 đến năm 2010.
+select * from film where release_year between 2000 and 2010;
+
+-- 3. Lấy danh sách các bộ phim (films) có rating là “PG” hoặc “PG-13”.
+select * from film where rating = 'PG' or rating = 'PG-13';
+
+-- 4. Lấy danh sách diễn viên (actors) và số lượng bộ phim (films) mà họ tham gia, sắp xếp theo số lượng bộ phim giảm dần.
+select actor.last_name, count(film_actor.film_id) as count from actor left join film_actor on actor.actor_id = film_actor.actor_id
+left join film on film_actor.film_id = film.film_id group by actor.actor_id
+order by count desc ;
+
+-- 5. Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng.
+select film.title, category.name from film join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id;
+
+-- 6. Lấy danh sách các bộ phim (films) và tổng số diễn viên (actors) tham gia trong mỗi bộ phim, sắp xếp theo tổng số diễn viên giảm dần.
+select film.title, count(fa.actor_id) as 'count_actor' from film left join sakila.film_actor fa on film.film_id = fa.film_id
+left join actor on fa.actor_id = actor.actor_id
+group by film.film_id
+order by count_actor desc;
+
+-- 7. Lấy danh sách các diễn viên (actors) có số lượng bộ phim (films) tham gia lớn hơn 30.
+select actor.last_name, count(film.film_id) as 'count_film' from actor left join film_actor on actor.actor_id = film_actor.actor_id
+left join film on film_actor.film_id = film.film_id group by actor.actor_id
+having count_film > 30;
+
+-- 8. Lấy danh sách các diễn viên (actors) không tham gia trong bất kỳ bộ phim nào.
+select actor.last_name, count(film.film_id) as 'count_film' from actor left join film_actor on actor.actor_id = film_actor.actor_id
+left join film on film_actor.film_id = film.film_id group by actor.actor_id
+having count_film = 0;
+
+-- 9. Lấy danh sách bộ phim (films) và tổng doanh thu (revenue) của từng bộ phim, sắp xếp theo tổng doanh thu giảm dần.
+select title, (rental_duration * rental_rate) as 'revenue'  from film
+order by revenue desc ;
+
+-- 10. Lấy danh sách các bộ phim (films) và thể loại (categories) của chúng, với điều kiện mỗi bộ phim thuộc thể loại “Horror”.
+select film.title, category.name from film join film_category on film.film_id = film_category.film_id
+join category on film_category.category_id = category.category_id
+where category.name = 'Horror';
